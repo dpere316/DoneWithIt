eda879b58590dbf9ad6c613ad9242eb3a2c32756
Daniel Perz <dpere316@fiu.edu>
2020-08-13 13:49:26 -0400

    Demo React Native App

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('App Executed')
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
