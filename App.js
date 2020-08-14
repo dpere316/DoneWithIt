071f2bce9d5d41f7906737cee2e60841541a94f8
Daniel Perz <dpere316@fiu.edu>
2020-08-14 16:25:56 -0400

    Creating Login Screen

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/assets/screens/WelcomeScreen';
 
 export default function App() {
-  console.log('App Executed')
+  
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen/>
   );
 }
 
@@ -20,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
---
+++ app/assets/screens/WelcomeScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
+
+function WelcomeScreen(props) {
+
+  return <ImageBackground 
+  style ={styles.background}
+  source={require("../splash.png")}>
+    {/* <Image/>  LOGO GOES HERE  */}
+      <View style={styles.loginButton}></View>
+      <View style={styles.signUpButton}></View>
+  </ImageBackground>;
+
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex:1,
+        justifyContent:"flex-end"
+    },
+    loginButton:{
+        width:"100%",
+        height:70,
+        backgroundColor:"#fc5c65"
+    },
+    signUpButton:{
+        width:"100%",
+        height:70,
+        backgroundColor:"#4ecdc4"
+    }
+})
+export default WelcomeScreen;
